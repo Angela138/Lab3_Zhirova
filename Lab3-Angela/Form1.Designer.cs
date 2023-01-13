1ae33d0512186cba646730666eab0f0dd488f43a
Angela138 <zhirova.anzhela@gmail.com>
2023-01-13 14:00:51 +0300

    Add new files

---
+++ Lab3-Angela/Form1.Designer.cs
@@ -0,0 +1,279 @@
+﻿namespace Lab3_Angela
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button_XOR = new System.Windows.Forms.Button();
+            this.button_clean = new System.Windows.Forms.Button();
+            this.groupBox_IN = new System.Windows.Forms.GroupBox();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textbox_C_IN = new System.Windows.Forms.TextBox();
+            this.textBox_Key_IN = new System.Windows.Forms.TextBox();
+            this.textBox_P_IN = new System.Windows.Forms.TextBox();
+            this.groupBox_OUT = new System.Windows.Forms.GroupBox();
+            this.label15 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.textBox_C_OUT = new System.Windows.Forms.TextBox();
+            this.textBox_Key_OUT = new System.Windows.Forms.TextBox();
+            this.textBox_P_OUT = new System.Windows.Forms.TextBox();
+            this.toolTip_HEX = new System.Windows.Forms.ToolTip(this.components);
+            this.groupBox_IN.SuspendLayout();
+            this.groupBox_OUT.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button_XOR
+            // 
+            this.button_XOR.Location = new System.Drawing.Point(179, 46);
+            this.button_XOR.Name = "button_XOR";
+            this.button_XOR.Size = new System.Drawing.Size(99, 51);
+            this.button_XOR.TabIndex = 0;
+            this.button_XOR.Text = "XOR";
+            this.button_XOR.UseVisualStyleBackColor = true;
+            this.button_XOR.Click += new System.EventHandler(this.button_XOR_Click);
+            // 
+            // button_clean
+            // 
+            this.button_clean.Location = new System.Drawing.Point(190, 129);
+            this.button_clean.Name = "button_clean";
+            this.button_clean.Size = new System.Drawing.Size(75, 30);
+            this.button_clean.TabIndex = 1;
+            this.button_clean.Text = "Прибрати";
+            this.button_clean.UseVisualStyleBackColor = true;
+            this.button_clean.Click += new System.EventHandler(this.button_clean_Click);
+            // 
+            // groupBox_IN
+            // 
+            this.groupBox_IN.Controls.Add(this.label12);
+            this.groupBox_IN.Controls.Add(this.label11);
+            this.groupBox_IN.Controls.Add(this.label10);
+            this.groupBox_IN.Controls.Add(this.label8);
+            this.groupBox_IN.Controls.Add(this.textbox_C_IN);
+            this.groupBox_IN.Controls.Add(this.textBox_Key_IN);
+            this.groupBox_IN.Controls.Add(this.textBox_P_IN);
+            this.groupBox_IN.Location = new System.Drawing.Point(12, 12);
+            this.groupBox_IN.Name = "groupBox_IN";
+            this.groupBox_IN.Size = new System.Drawing.Size(161, 147);
+            this.groupBox_IN.TabIndex = 2;
+            this.groupBox_IN.TabStop = false;
+            this.groupBox_IN.Text = "Зашифрування";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(17, 119);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(23, 13);
+            this.label12.TabIndex = 6;
+            this.label12.Text = "C =";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(6, 72);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(34, 13);
+            this.label11.TabIndex = 5;
+            this.label11.Text = "Key =";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(17, 26);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(23, 13);
+            this.label10.TabIndex = 4;
+            this.label10.Text = "P =";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(43, 34);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(0, 13);
+            this.label8.TabIndex = 3;
+            // 
+            // textbox_C_IN
+            // 
+            this.textbox_C_IN.Location = new System.Drawing.Point(46, 117);
+            this.textbox_C_IN.Name = "textbox_C_IN";
+            this.textbox_C_IN.Size = new System.Drawing.Size(100, 20);
+            this.textbox_C_IN.TabIndex = 2;
+            this.textbox_C_IN.TextChanged += new System.EventHandler(this.textbox_C_IN_TextChanged);
+            // 
+            // textBox_Key_IN
+            // 
+            this.textBox_Key_IN.Location = new System.Drawing.Point(46, 72);
+            this.textBox_Key_IN.Name = "textBox_Key_IN";
+            this.textBox_Key_IN.Size = new System.Drawing.Size(100, 20);
+            this.textBox_Key_IN.TabIndex = 1;
+            // 
+            // textBox_P_IN
+            // 
+            this.textBox_P_IN.Location = new System.Drawing.Point(46, 26);
+            this.textBox_P_IN.Name = "textBox_P_IN";
+            this.textBox_P_IN.Size = new System.Drawing.Size(100, 20);
+            this.textBox_P_IN.TabIndex = 0;
+            // 
+            // groupBox_OUT
+            // 
+            this.groupBox_OUT.Controls.Add(this.label15);
+            this.groupBox_OUT.Controls.Add(this.label14);
+            this.groupBox_OUT.Controls.Add(this.label13);
+            this.groupBox_OUT.Controls.Add(this.label9);
+            this.groupBox_OUT.Controls.Add(this.textBox_C_OUT);
+            this.groupBox_OUT.Controls.Add(this.textBox_Key_OUT);
+            this.groupBox_OUT.Controls.Add(this.textBox_P_OUT);
+            this.groupBox_OUT.Location = new System.Drawing.Point(284, 12);
+            this.groupBox_OUT.Name = "groupBox_OUT";
+            this.groupBox_OUT.Size = new System.Drawing.Size(168, 156);
+            this.groupBox_OUT.TabIndex = 3;
+            this.groupBox_OUT.TabStop = false;
+            this.groupBox_OUT.Text = "Розшифрування";
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Location = new System.Drawing.Point(126, 118);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(23, 13);
+            this.label15.TabIndex = 6;
+            this.label15.Text = "= C";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(125, 73);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(34, 13);
+            this.label14.TabIndex = 5;
+            this.label14.Text = "= Key";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(126, 27);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(23, 13);
+            this.label13.TabIndex = 4;
+            this.label13.Text = "= P";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(16, 34);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(0, 13);
+            this.label9.TabIndex = 3;
+            // 
+            // textBox_C_OUT
+            // 
+            this.textBox_C_OUT.Location = new System.Drawing.Point(19, 118);
+            this.textBox_C_OUT.Name = "textBox_C_OUT";
+            this.textBox_C_OUT.Size = new System.Drawing.Size(100, 20);
+            this.textBox_C_OUT.TabIndex = 2;
+            // 
+            // textBox_Key_OUT
+            // 
+            this.textBox_Key_OUT.Location = new System.Drawing.Point(19, 73);
+            this.textBox_Key_OUT.Name = "textBox_Key_OUT";
+            this.textBox_Key_OUT.Size = new System.Drawing.Size(100, 20);
+            this.textBox_Key_OUT.TabIndex = 1;
+            // 
+            // textBox_P_OUT
+            // 
+            this.textBox_P_OUT.Location = new System.Drawing.Point(19, 27);
+            this.textBox_P_OUT.Name = "textBox_P_OUT";
+            this.textBox_P_OUT.Size = new System.Drawing.Size(100, 20);
+            this.textBox_P_OUT.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(480, 334);
+            this.Controls.Add(this.groupBox_OUT);
+            this.Controls.Add(this.groupBox_IN);
+            this.Controls.Add(this.button_clean);
+            this.Controls.Add(this.button_XOR);
+            this.Name = "Form1";
+            this.groupBox_IN.ResumeLayout(false);
+            this.groupBox_IN.PerformLayout();
+            this.groupBox_OUT.ResumeLayout(false);
+            this.groupBox_OUT.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.Button button_XOR;
+        private System.Windows.Forms.Button button_clean;
+        private System.Windows.Forms.GroupBox groupBox_IN;
+        private System.Windows.Forms.TextBox textbox_C_IN;
+        private System.Windows.Forms.TextBox textBox_Key_IN;
+        private System.Windows.Forms.TextBox textBox_P_IN;
+        private System.Windows.Forms.GroupBox groupBox_OUT;
+        private System.Windows.Forms.TextBox textBox_C_OUT;
+        private System.Windows.Forms.TextBox textBox_Key_OUT;
+        private System.Windows.Forms.TextBox textBox_P_OUT;
+        private System.Windows.Forms.ToolTip toolTip_HEX;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label9;
+    }
+}
+
---
+++ Lab3-Angela/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab3_Angela
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        void myShowToolTip(TextBox tB, byte[] arr)
+        {
+            string hexValues = BitConverter.ToString(arr);
+            String hexvv = null;
+            int a = 0;
+            foreach (char c in hexValues)
+            {
+                a = Convert.ToInt32(c);
+                hexvv += Convert.ToString(a, 2);
+            }
+            toolTip_HEX.SetToolTip(tB, hexvv);
+        }
+        byte[] myXOR(byte[] arr_text, byte[] arr_key)
+        {
+            int len_text = arr_text.Length;
+            int len_key = arr_key.Length;
+            byte[] arr_cipher = new byte[len_text];
+            for (int i = 0; i < len_text; i++)
+            {
+                byte p = arr_text[i];
+                byte k = arr_key[i % len_key]; // mod
+                byte c = (byte)(p ^ k); // XOR
+
+                arr_cipher[i] = c;
+            }
+            return arr_cipher;
+        }
+        string myCipher(TextBox tb_text, TextBox tb_Key, TextBox tb_cipher, string cipher = "")
+        {
+            string text = tb_text.Text;
+            byte[] arr_text;
+            if (cipher == "") arr_text = Encoding.UTF8.GetBytes(text);
+            else arr_text = Encoding.UTF8.GetBytes(cipher);
+            myShowToolTip(tb_text, arr_text); // Створити підказку
+
+            string key = tb_Key.Text;
+            byte[] arr_key = Encoding.UTF8.GetBytes(key);
+            myShowToolTip(tb_Key, arr_key); // Створити підказку
+
+            byte[] arr_cipher = myXOR(arr_text, arr_key);
+
+            //string cipher = BitConverter.ToString(arr_cipher).Replace(&quot;-&quot;, &quot;&quot;);
+            cipher = Encoding.UTF8.GetString(arr_cipher);
+            tb_cipher.Text = cipher;
+            myShowToolTip(tb_cipher, arr_cipher); // Створити підказку
+
+            return cipher;
+        }
+        private void button_XOR_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(textBox_Key_IN.Text))
+            {
+                textBox_P_OUT.Text = textBox_P_IN.Text;
+                textBox_C_OUT.Text = textBox_P_IN.Text;
+                textbox_C_IN.Text = textBox_P_IN.Text;
+                return;
+            }
+            string cipher = myCipher(textBox_P_IN, textBox_Key_IN, textbox_C_IN); //зашифрування
+            textBox_P_OUT.Text = textbox_C_IN.Text;
+            textBox_Key_OUT.Text = textBox_Key_IN.Text;
+            myCipher(textBox_C_OUT, textBox_Key_OUT, textBox_C_OUT, cipher); //розшифрування
+        }
+        private void button_clean_Click(object sender, EventArgs e)
+        {
+            textBox_P_IN.Text = "";
+            textBox_Key_IN.Text = "";
+            textbox_C_IN.Text = "";
+
+            textBox_P_OUT.Text = "";
+            textBox_Key_OUT.Text = "";
+            textBox_C_OUT.Text = "";
+        }
+
+        private void textbox_C_IN_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
